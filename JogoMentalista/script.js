94c37d5003f7921da1329ce04b2be8b018d5b7a9
Deyvison Moraes <moraes.deyvison20@gmail.com>
2022-09-28 22:59:19 -0400

    rearranging the source code

--- JogoMentalista/script.js
+++ JogoMentalista/script.js
@@ -1,15 +1,25 @@
-var numeroSecreto = parent(Math.random() * 11);
+var tentativas = 1;
+var numeroSecreto = parseInt(Math.random() * 11);
 
 function Chutar() {
-	    var numeroSecreto = parseInt(Math.random() * 11);
-	    var elementoResultado = document.getElementById("resultado");
-	    var chute = parseInt(document.getElementById("valor").value);
-	    if (chute == numeroSecreto) {
-		            elementoResultado.innerHTML = "Você acertou";
-		        } else if (chute > 10 || chute < 0) {
-				        elementoResultado.innerHTML = "Você deve digitar um número de 0 a 10";
-				    } else {
-					            elementoResultado.innerHTML = "Errou, o número secreto era" + numeroSecreto;
-					            elementoResultado.innerHTML = "Errou, o número secreto era " + numeroSecreto;
-					        }
+    var elementoResultado = document.getElementById("resultado");
+    var chute = document.getElementById("valor");
+    var valorChute = parseInt(chute.value);
+    var btn = document.getElementById("btn_chute")
+
+    elementoResultado.innerHTML = "";
+
+    if (valorChute > 10 || valorChute < 0) {
+        elementoResultado.innerHTML = "Você deve digitar um número de 0 a 10";
+    } else if (valorChute == numeroSecreto && tentativas <= 3) {
+        elementoResultado.innerHTML = `Você acertou na ${tentativas}º tentativa, recarregue a pagina!!!`;
+        chute.readOnly = true;
+        btn.disabled = true;
+    } else if (tentativas >= 3) {
+        elementoResultado.innerHTML = `Errou, o número secreto era ${numeroSecreto}, recarregue a pagina!!!`;
+        chute.readOnly = true;
+        btn.disabled = true;
+    } else {
+        tentativas++;
+    }
 }
