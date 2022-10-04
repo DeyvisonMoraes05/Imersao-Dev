25f09a8d9d6c35a059cb62e9d5f0d8174d9e0e86
Deyvison Moraes <moraes.deyvison20@gmail.com>
2022-10-04 19:54:57 -0400

    Adding  greater than and less than function

--- JogoMentalista/script.js
+++ JogoMentalista/script.js
@@ -6,11 +6,13 @@ function Chutar() {
     var chute = document.getElementById("valor");
     var valorChute = parseInt(chute.value);
     var btn = document.getElementById("btn_chute")
+    var paragrafo = document.getElementById("exibir")
 
     elementoResultado.innerHTML = "";
 
-    if (valorChute > 10 || valorChute < 0) {
-        elementoResultado.innerHTML = "Você deve digitar um número de 0 a 10";
+    // Verificar se o número ta dentro do valor especificado
+    if (valorChute > 10 || valorChute < 0 || typeof(valorChute) == NaN) {
+        elementoResultado.innerHTML = "Você deve digitar um número entre 0 e 10";
     } else if (valorChute == numeroSecreto && tentativas <= 3) {
         elementoResultado.innerHTML = `Você acertou na ${tentativas}º tentativa, recarregue a pagina!!!`;
         chute.readOnly = true;
@@ -20,6 +22,11 @@ function Chutar() {
         chute.readOnly = true;
         btn.disabled = true;
     } else {
+        if (valorChute > numeroSecreto) {
+            paragrafo.innerHTML = `O número secreto é menor que ${valorChute}`;
+        } else {
+            paragrafo.innerHTML = `o número secreto é maior que ${valorChute} `;
+        }
         tentativas++;
     }
 }
