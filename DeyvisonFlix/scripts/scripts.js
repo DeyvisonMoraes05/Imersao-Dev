d94e7dcf3d99cf06be3b079d5352825519c6d65a
Deyvison Moraes <moraes.deyvison20@gmail.com>
2022-10-17 20:35:09 -0400

    Formatting page function

--- DeyvisonFlix/scripts/scripts.js
+++ DeyvisonFlix/scripts/scripts.js
@@ -1,1 +1,14 @@
-var listaFilmes = []
+var listaFilmes = [
+    `./images/${"1pedra.jpg"}`,
+    `./images/${"2camara.jpg"}`,
+    `./images/${"3prisioneiro.jpg"}`,
+    `./images/${"4calice.jpg"}`,
+    `./images/${"5ordem.jpg"}`,
+    `./images/${"6enigma.jpg"}`,
+    `./images/${"7.0reliquias1.jpg"}`,
+    `./images/${"7.1reliquias2.jpg"}`
+]
+
+for (var i = 0; i < listaFilmes.length; i++) {
+    document.write("<img src=" + listaFilmes[i] + ">")
+}
